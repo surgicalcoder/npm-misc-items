94004339b16a1513c9aa7676208d2562f7b2fc80
Monty <ma@surgicalcoder.com>
2019-09-22 19:23:55 +0100

    Added date-ago pipe

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export {MiscModule} from './misc.module';
 export {StateService} from './stateservice'
+export {DateAgoPipe} from './date-ago.pipe'
 export {PagedResponse, Ref, toItem, WeakRef, Entity, EncryptedString, HashedString, ExternallyPopulatedEntity, ScopedEntity, Timestamp} from './PagedResponse';
--- src/misc.module.ts
+++ src/misc.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import {CommonModule} from '@angular/common'
+import {DateAgoPipe} from './date-ago.pipe'
 
 @NgModule({
 	imports: [CommonModule],
+	declarations: [DateAgoPipe],
+	exports: [DateAgoPipe]
 })
 
 export class MiscModule { }
